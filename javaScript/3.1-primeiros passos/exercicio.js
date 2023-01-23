f620a02ddb88a16a3d1c2d5d3384b420215950fb
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-23 13:59:42 -0300

    Variavel

--- javaScript/3.1-primeiros passos/exercicio.js
+++ javaScript/3.1-primeiros passos/exercicio.js
@@ -1,1 +1,13 @@
-oi 
+//Exercícios de javascript!
+
+
+let nome = "";
+let age = 25;
+
+if (nome === "Keyla") {
+    console.log("Meu nome é keyla");
+} else if (nome === 'Bruna') {
+    console.log("Meu nome é Bruna");
+} else {
+    console.log("Nome não definido");
+}
