7525a9146061d739f8963d14d1155e5ae7133986
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-23 18:05:43 -0300

    Adicionando atividades do dia

--- javaScript/3.1-primeiros passos/exercicio.js
+++ javaScript/3.1-primeiros passos/exercicio.js
@@ -1,13 +1,67 @@
 //Exercícios de javascript!
 
+/*const idade1 = 20;
+const idade2 = 30;
+const idade3 = 55;
 
-let nome = "";
-let age = 25;
+console.log('adição:' + (idade1 + idade2));
+console.log('subtração:' + (idade1 - idade2));
+console.log('multiplicação:' + (idade1 * idade2));
+console.log('divisão:' + (idade1 / idade2));
+console.log('módulo:' + (idade1 % idade2));*/
 
-if (nome === "Keyla") {
-    console.log("Meu nome é keyla");
-} else if (nome === 'Bruna') {
-    console.log("Meu nome é Bruna");
+
+/*const idade1 = 20;
+const idade2 = 30;
+
+
+if (idade1 > idade2) {
+    console.log("A maior idade é" + idade1);
+} else if (idade2 > idade1 ) {
+    console.log("A maior idade é" + ' ' + idade2);
 } else {
-    console.log("Nome não definido");
-}
+    console.log('Não possui idade maior!');
+}*/
+
+/*const idade1 = 20;
+const idade2 = 20;
+const idade3 = idade1 + idade2;
+
+
+if (idade1 > idade2) {
+    console.log("A maior idade é" + idade1);
+} else if (idade2 > idade1 ) {
+    console.log("A maior idade é" + ' ' + idade2);
+} else if (idade1 + idade1 === 40) {
+    console.log("A maior idade é" + ' ' + idade3); 
+} else {
+    console.log('Não possui idade maior!');
+}*/
+
+/*const number = 4;
+
+if (number > 0) {
+  console.log('positive');
+} else if (number < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+};*/
+
+/* Áreas de um triangulo - boolean
+const area1 = 40;
+const area2 = 90;
+const area3 = 50;
+
+let somaDasAreas = area1 + area2 + area3;
+
+if (somaDasAreas === 180) {
+    console.log(true)
+} else {
+    console.log(false);
+  }; 
+}  else {
+  console.log('Erro: ângulo inválido');
+}*/
+
+
---
+++ javaScript/3.1-primeiros passos/teste1.js
@@ -0,0 +1,19 @@
+/* 
+   Agora vamos ter 15 minutos para construir um algoritmo com a seguinte descrição:
+   Crie dois valores num1 e num2.
+   Se os valores forem iguais some os dois
+   Caso contrário multiplique num1 por num2.
+   Por ultimo exiba o valor final no console.
+*/
+
+
+const num1 = 2;
+const num2 = 10;
+const soma = num1 + num2
+
+
+if (num1 === num2) {
+    console.log(num1 + num2)
+} else (num1 !== num2); {
+    console.log (num1 * num2)
+}
