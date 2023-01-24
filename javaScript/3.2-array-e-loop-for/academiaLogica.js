4f08a22480a9105cf4b795926f88a267e8c4ebea
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-24 14:00:05 -0300

    adicionado mais atividades

--- javaScript/3.2-array-e-loop-for/academiaLogica.js
+++ javaScript/3.2-array-e-loop-for/academiaLogica.js
@@ -13,3 +13,45 @@ if (marina <  silvia && marina <  iza) {
 } else {
     console.log("Idades inseridas não válidas.");
 }
+
+/* Taxa Metabólica Basal */
+
+let age = 24
+let sex = 'F'
+let weight = 60
+let heigh = 163
+
+  let bmr;
+
+  if (sex === 'M') {
+    bmr = heigh * 6.25 + weight * 9.99 - age * 4.92 + 5;
+  }
+
+  if (sex === 'F') {
+    bmr = heigh * 6.25 + weight * 9.99 - age * 4.92 - 161;
+  }
+
+  console.log('A taxa metabólica basal é: ' + bmr + ' Kcal') 
+
+
+/*  01 - Lanchonete da Trybe */
+let number = 4
+  switch (number) {
+    case 1:
+      console.log('1 - Trybe Lanche Feliz');
+      break
+    case 2:
+      console.log('2 - McTrybe');
+      break
+    case 3:
+      console.log('3 - TrybeWooper');
+      break
+    case 4:
+      console.log('4 - X-Trybe');
+      break
+    case 5:
+     console.log('5 - Triplo Trybe com JS');
+     break
+    default:
+      console.log('Não temos esta opção ainda :(');
+  }
