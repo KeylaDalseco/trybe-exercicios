485fc770ca4d26744f57ecee29c70d3f24b5e37e
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-25 21:46:22 -0300

    adicionado exercicio flavio

---
+++ javaScript/3.2-array-e-loop-for/academiaDeLogica.js
@@ -0,0 +1,23 @@
+/* 2. Retorne os números ímpares
+Primeiramente divida seu problema em partes e só depois disso comece a codar.
+
+A Dora aventureira gostaria de ter uma lista com todos o números ímpares entre 1 e 50.
+
+Escreva um algoritmo que imprima no terminal uma string com todos os números inteiros ímpares desse intervalo.
+
+Use a seguinte estrutura na impressão:
+
+"x, y, z, ...n" */
+
+/*
+1 - Criar uma lista com todos os números ímpares entre 1 e 50;
+2 - Estruturar o for;
+
+
+
+*/
+
+let numerosImpares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+
+
+
