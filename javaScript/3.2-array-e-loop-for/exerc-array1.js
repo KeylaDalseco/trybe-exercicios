91b3fe361f4264f1ee93667cb37b4d25ac9a120b
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-25 23:24:39 -0300

    exercicio q mo me ajudou

--- javaScript/3.2-array-e-loop-for/exerc-array1.js
+++ javaScript/3.2-array-e-loop-for/exerc-array1.js
@@ -1,12 +1,12 @@
 //somar os numeros do array
 
-/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
-let result = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
+// let result = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    result += numbers[index];
-}
-console.log(result);*/
+// for (let index = 0; index < numbers.length; index += 1) {
+//     result += numbers[index];
+// }
+// console.log(result);
 
 //ACHAR MEDIA ARITMETICA E SABER SE O VALOR É MAIOR QUE 20 OU NÃO
 
---
+++ javaScript/3.2-array-e-loop-for/testando1.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    result = numbers[index] * 3;
+    console.log(result);
+} 
+let media = result / numbers.length;
+console.log(media);
+
+if(media > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
