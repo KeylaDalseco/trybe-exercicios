31f2fa80c08cb80c22711a116f0dbab9b45820e3
KeylaDalseco <keylacostadalseco@gmail.com>
2023-01-26 23:45:54 -0300

    exercicio maior palavra adicionado

---
+++ javaScript/exerc1.js
@@ -0,0 +1,26 @@
+// let fatorial = 1;
+
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+//palavra invertida
+
+// let word1 = 'tryber';
+// let inverse = '';
+
+// for (let index = 0; index < word1.length; index++) {
+//     inverse += word1[word1.length - 1 - index];
+    
+// }
+// console.log(inverse);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = 0;
+
+for (let index = 0; index < array.length; index+=1) {
+    maiorPalavra += array.length;
+    console.log(maiorPalavra);
+}
