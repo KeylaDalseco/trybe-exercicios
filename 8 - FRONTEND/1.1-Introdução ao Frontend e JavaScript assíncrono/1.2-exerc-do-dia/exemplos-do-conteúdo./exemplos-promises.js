41f5e1f87a3fbade53695a919098b3c83a92424b
KeylaDalseco <keylacostadalseco@gmail.com>
2023-03-17 20:39:55 -0300

    atualizando conteúdo do dia 1.2

---
+++ 8 - FRONTEND/1.1-Introdução ao Frontend e JavaScript assíncrono/1.2-exerc-do-dia/exemplos-do-conteúdo./exemplos-promises.js
@@ -0,0 +1,41 @@
+// função que gera um número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+// promise resolvida retornando o número aleatório
+const resolvedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      resolve(randomNumber);
+    }, 1000);
+  });
+
+// promise rejeitada retornando um objeto de erro
+const rejectedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      reject(new Error(`O número ${randomNumber} é inválido.`));
+    }, 1000);
+  });
+
+// o recomendado é usar as 2 juntas (resolve e reject) - com ifs e tals
+
+
+resolvedPromise()
+  .then((response) => {
+    console.log(`resolvedPromise: o número gerado é ${response}.`);
+  })
+  .catch((response) => {
+    console.log(`Com a promise resolvida, não irá passar pelo catch`);
+  })
+  .finally(() => console.log('Fim da execução da primeira promise.'));
+
+rejectedPromise()
+  .then((response) => {
+    console.log(`Com a promise rejeitada, não irá passar pelo then`);
+  })
+  .catch((error) => {
+    console.log(`rejectedPromise: ${error.message}`);
+  })
+  .finally(() => console.log('Fim da execução da segunda promise.')); // Caso seja necessário executar algum código após o retorno da promise, independentemente se ela foi resolvida ou rejeitada, podemos usar o método finally. Esse método recebe como parâmetro uma função, que não recebe nenhum argumento. 
